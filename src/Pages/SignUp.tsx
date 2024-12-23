2d35175a7e0b361d0ee380b440034dd7c6e3764d
ahmadrazanawab <ahmadrazanawab@gmail.com>
2024-12-23 21:26:24 +0530

    sign up design update

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -32,24 +32,24 @@ const SignUp: React.FC = () => {
     }
   return (
     <div className='w-full bg-[#f1f2f3]'>
-          <div className='flex flex-col justify-center items-center py-10'>
+          <div className='flex flex-col justify-center items-center py-2'>
               <form action="" onSubmit={handleSubmit} className='flex flex-col md:w-[50%] xl:w-[30%] w-[60%] my-2 bg-white shadow-md p-6 rounded'>
-                <h4 className='text-xl py-3 text-center text-gray-900 font-serif'>Sign Up Form</h4>
+                <h4 className='text-xl py-1 text-center text-gray-900 font-serif'>Sign Up Form</h4>
                   <label htmlFor="name">Name</label>
-                  <input type="text" value={credentials.name} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name='name' id="name" placeholder='Enter your name' />
+                  <input type="text" value={credentials.name} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name='name' id="name" placeholder='Enter your name' minLength={3} />
                   <label htmlFor="email">Email</label>
                   <input type="email" value={credentials.email} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="email" id="email" placeholder='Enter your email'/>
                   <label htmlFor="number">Number</label>
-                  <input type="number" value={credentials.number} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="number" id="number" placeholder='Enter your mobile number' />
+                  <input type="number" value={credentials.number} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="number" id="number" placeholder='Enter your mobile number' minLength={10} maxLength={10} />
                   <label htmlFor="password">Password</label>
-                  <input type="password" value={credentials.password} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="password" id="password" placeholder='Enter your password'/>
+                  <input type="password" value={credentials.password} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="password" id="password" placeholder='Enter your password' minLength={6}/>
                   <label htmlFor="cpassword">Confirm Password</label>
-                  <input type="password" value={credentials.cpassword} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="cpassword" id="cpassword" placeholder='Enter your confirm password'/>
+                  <input type="password" value={credentials.cpassword} onChange={onChange} className='px-1 py-1 mb-1 border-[1px] border-gray-900 rounded outline-none' name="cpassword" id="cpassword" placeholder='Enter your confirm password' minLength={6}/>
                   <p className='tracking-tighter text-sm mb-4 text-gray-500'>Passwords must be contain at least six charecters</p>
                   <label htmlFor="check" className='flex items-center'><input type="checkbox" name="check" id="check" required /><p className='text-sm mx-2'>I agree to the <span className='text-[#018ce3]'>Terms and Conditions</span></p></label>
                   <button type='submit' className='border-[1px] my-3 border-gray-900 px-2 py-1 cursor-pointer bg-[#009dff] text-white rounded'>Sign Up</button>
               </form>
-              <p className='text-gray-900 my-6  font-sans'>already have an account? <Link to='/login' className='text-[#007ac6] underline tracking-tighter'>Login</Link> </p>
+              <p className='text-gray-900 my-2  font-sans'>already have an account? <Link to='/login' className='text-[#007ac6] underline tracking-tighter'>Login</Link> </p>
        </div>
     </div>
   )
